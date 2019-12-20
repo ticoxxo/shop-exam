e7e9364f4852594cb100dd989c5f7ad832fe9c71
webdevabordo <jalmeida@allabordo.com>
2019-12-19 17:49:23 -0700

    added client role based

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,11 +28,11 @@ class ProductController extends Controller
     {
 
         //  $this->middleware('permission:product-list|product-create|product-edit|product-delete', ['only' => ['index','show']]);
-         $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
+         $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','show']]);
          $this->middleware('permission:product-create', ['only' => ['create','store']]);
 
          $this->middleware('permission:product-edit', ['only' => ['edit','update']]);
-
+         $this->middleware('permission:client', ['only' => ['','']]);
          $this->middleware('permission:product-delete', ['only' => ['destroy']]);
 
     }
